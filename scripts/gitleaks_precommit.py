a2a3e92edf0413be90d8fad42102ce04ead3e69c
Tony <48512979+fedor22i@users.noreply.github.com>
2025-07-04 15:39:53 +0000

    Test secret5

--- scripts/gitleaks_precommit.py
+++ scripts/gitleaks_precommit.py
@@ -93,21 +93,10 @@ def run_gitleaks():
         print("Немає змінених файлів у дозволених директоріях.")
         return
 
-    with tempfile.TemporaryDirectory() as tmpdir:
-        for f in files:
-            dest_path = os.path.join(tmpdir, f)
-            os.makedirs(os.path.dirname(dest_path), exist_ok=True)
-            try:
-                with open(dest_path, "wb") as out:
-                    subprocess.run(["git", "show", f":{f}"], stdout=out, check=True)
-            except subprocess.CalledProcessError:
-                print(f"Не вдалося отримати staged-версію файлу: {f}")
-                sys.exit(1)
-
         try:
             subprocess.run([
                 "gitleaks", "detect",
-                "--source", tmpdir,
+                "--source", folder,
                 "--no-git",
                 "--no-banner"
             ], check=True)
